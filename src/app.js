73aff6cfc720efd7f4e94d7d9555a171fa077c14
Henry Heikkinen <rce@rce.fi>
2020-03-01 13:27:30 +0200

    Transform timestamps to local times and add few events

--- src/app.js
+++ src/app.js
@@ -1,28 +1,39 @@
 const express = require("express")
 const events = require("./events.js")
 
+const { IANAZone } = require("luxon")
+
 const contentType = process.env.CONTENT_TYPE || "text/plain"
 
 const app = express()
 
 app.get("/events.ics", (req, res) => {
+  const zone = IANAZone.create(req.query.tz)
+  if (!zone.valid) {
+    res.status(400).send("Bad Request")
+    return
+  }
+
   res.append('Content-Type', contentType)
 
   writeField(res, "BEGIN", "VCALENDAR")
   writeField(res, "VERSION", "2.0")
   writeField(res, "PRODID", "-//github.com/rce//Pokemon GO Events V1.0//EN")
   writeField(res, "X-WR-CALNAME", "Pokemon GO Event Calendar")
-  events.forEach(event => writeEvent(res, event))
+  events.forEach(event => writeEvent(res, event, zone))
   writeField(res, "END", "VCALENDAR")
   res.end()
 })
 
-function writeEvent(res, event) {
+function writeEvent(res, event, zone) {
+  const mkStamp = dt =>
+      calDateTime(dt.setZone(zone, { keepLocalTime: event.localTime }))
+
   writeField(res, "BEGIN", "VEVENT")
   writeField(res, "UID", event.id)
-  writeField(res, "DTSTAMP", event.created)
-  writeField(res, "DTSTART", event.start)
-  writeField(res, "DTEND", event.end)
+  writeField(res, "DTSTAMP", "19700101T000000Z")
+  writeField(res, "DTSTART", mkStamp(event.start))
+  writeField(res, "DTEND", mkStamp(event.end))
   writeField(res, "SUMMARY", event.title)
   writeField(res, "DESCRIPTION", event.title)
   writeField(res, "CLASS", "PUBLIC")
@@ -31,6 +42,11 @@ function writeEvent(res, event) {
 
 function writeField(res, key, value) {
   res.write(key + ":" + value + "\r\n")
+}
+
+function calDateTime(original) {
+  const dt = original.toUTC()
+  return dt.toFormat("yyyyMMdd") + "T" + dt.toFormat("HHmm00") + "Z"
 }
 
 module.exports = app
--- src/events.js
+++ src/events.js
@@ -1,10 +1,47 @@
+const { DateTime, IANAZone } = require("luxon")
+
+const pokemonGoTimeZone = IANAZone.create("UTC")
+
 module.exports = [
   {
-    id: "altered-forme-giratina-weekend",
-    created: "20200301T104300Z",
-    title: "Altered forme giratine special raid weekend",
-    start: "20200313T080000Z",
-    end: "20200316T100000Z",
+    id: "pokemon-day-raid-day-2020",
+    title: "Pokémon Day Raid Day 2020",
+    start: parseTime("2020-03-01 14:00"),
+    end: parseTime("2020-03-01 17:00"),
+    localTime: true,
+  },
+  {
+    id: "pokemon-spotlight-hour",
+    title: "Pokémon Spotlight Hour",
+    start: parseTime("2020-03-03 18:00"),
+    end: parseTime("2020-03-03 19:00"),
+    localTime: true,
+  },
+  {
+    id: "mystery-bonus-hour",
+    title: "Mystery Bonus Hour",
+    start: parseTime("2020-03-05 18:00"),
+    end: parseTime("2020-03-05 19:00"),
+    localTime: true,
+  },
+  {
+    id: "darkrai-special-raid-weekend",
+    title: "Darkrai Special Raid Weekend",
+    start: parseTime("2020-03-06 08:00"),
+    end: parseTime("2020-03-09 22:00"),
+    localTime: true,
+  },
+  {
+    id: "go-rocket-global-takeover",
+    title: "GO Rocket Global Takeover",
+    start: parseTime("2020-03-07 14:00"),
+    end: parseTime("2020-03-07 17:00"),
     localTime: true,
   },
 ]
+
+function parseTime(text) {
+  return DateTime.fromFormat(text, "yyyy-MM-dd HH:mm", {
+    setZone: pokemonGoTimeZone,
+  })
+}
